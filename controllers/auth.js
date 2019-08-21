d29c6a7ab9c083c97bc0c5cf00e25c069d738bb3
Donald Nyingifa <donaldnyingifa@gmail.com>
2019-08-21 11:52:00 +0100

    Added authentication and some other changes

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,29 +1,112 @@
 const User = require('../models/user');
+const Login = require('../models/login');
+const sequelize = require('../util/database');
+const bcrypt =  require('bcrypt');
+const saltRounds = 10;
 
 exports.getLogin = (req, res, next) => {
+  //const isLoggedIn = req.get('Cookie').split(';')[3].trim().split('=')[1];
+   // console.log('log check',req.get('Cookie').split(';')[3].trim().split('=')[1])
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login',
+   // isAuthenticated: isLoggedIn
     isAuthenticated: false
   });
 };
 
 exports.postLogin = (req, res, next) => {
-  User.findById('5bab316ce0a7c75f783cb8a8')
-    .then(user => {
-      req.session.isLoggedIn = true;
-      req.session.user = user;
-      req.session.save(err => {
-        console.log(err);
-        res.redirect('/');
-      });
+
+ const {email, password} = req.body;
+
+  Login.findAll({where: {email: email}})
+    .then(data => {
+      if (!data.length) return res.status(400).json("Email or Password is invalid")
+      const isValid = bcrypt.compareSync(password, data[0].hash);
+      if (isValid) {
+        return User.findAll({where: {email: email}})
+        .then(user => {
+          req.session.isLoggedIn = true;
+          req.session.user = user;
+          req.session.save(err => {
+            console.log(err);
+            res.redirect('/');
+          });
+        })
+      } else res.status(400).json("Invalid Details")
     })
     .catch(err => console.log(err));
+};
+
+exports.postSignup = (req, res, next) => {
+  const {email, password} = req.body
+  const hash = bcrypt.hashSync(password, saltRounds);
+
+  User.findAll({where: {email: email}})
+  .then(user => {
+    if (!user.length) {
+      return User.create({
+        email: email, 
+        password: hash,
+        })
+        .then(user => {
+          Login.create({ email: email, hash: hash })
+          user.createCart();
+          return user;
+        })
+        .then(user => {
+          req.session.isLoggedIn = true;
+          req.session.user = user;
+          req.session.save(err => {
+            console.log(err);
+            res.redirect('/');
+          });
+        })
+    } else {
+      res.status(400).json("Email Already Exists")
+    }
+  }) 
+    .catch(err => {
+      console.log(err);
+        });
+   
+  // return sequelize.transaction(function (t) {
+
+  //   // chain all your queries here. make sure you return them.
+  //   return User.create({
+  //        email: email, 
+  //       password: hash,
+  //   }, {transaction: t})
+  //   .then(function (user) {
+  //     console.log(user, '----------------')
+  //     Login.create({ email: email, hash: hash }
+  //       , {transaction: t});
+  //     return user
+  //   }, {transaction: t})
+  
+  // }).then(function (user) {
+  //   user.createCart();
+  //   // Transaction has been committed
+  //   // result is whatever the result of the promise chain returned to the transaction callback
+  //   res.redirect('/');
+  // }).catch(function (err) {
+  //   // Transaction has been rolled back
+  //   // err is whatever rejected the promise chain returned to the transaction callback
+  //   console.log(err)
+  // });
 };
 
 exports.postLogout = (req, res, next) => {
   req.session.destroy(err => {
     console.log(err);
     res.redirect('/');
+  });
+};
+
+exports.getSignup = (req, res, next) => {
+  res.render('auth/signup', {
+    path: '/signup',
+    pageTitle: 'Signup',
+    isAuthenticated: false
   });
 };
--- server.js
+++ server.js
@@ -2,16 +2,24 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const session = require('express-session');
 
 const errorController = require('./controllers/error');
-const sequelize = require('./util/database')
+const sequelize = require('./util/database');
+
+const cookieParser = require('cookie-parser')
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const Product = require('./models/product');
 const User = require('./models/user');
+const Login = require('./models/login');
 const Cart = require('./models/cart');
 const CartItem = require('./models/cart-item');
 const Order = require('./models/order');
 const OrderItem = require('./models/order-item');
+
+const bcrypt =  require('bcrypt');
+const saltRounds = 10;
 
 const app = express();
 
@@ -28,13 +36,26 @@ const authRoutes = require('./routes/auth');
 //   console.log(data)
 // })
 
+app.use(cookieParser())
+
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+    resave: false, // we support the touch method so per the express-session docs this should be set to false
+    proxy: true, // if you do SSL outside of node.
+    store: new SequelizeStore({
+        db: sequelize
+      }),
+    secret: 'my secret', 
+    resave: false, 
+    saveUninitialized: false,
+    cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 } // 30 days
+}))
 
 app.use((req, res, next) => {
     User.findAll({where: {id: 1}})
     .then(([user]) => {
-        console.log('hello', user);
+        //console.log('hello', user);
         req.user = user;
         next();
     })
@@ -46,11 +67,11 @@ app.use(shopRoutes);
 app.use(authRoutes);
 
 app.use(errorController.get404);
-
 Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE'});
 User.hasMany(Product);
 User.hasOne(Cart);
 Cart.belongsTo(User);
+//Login.belongsTo(User);
 Cart.belongsToMany(Product, { through: CartItem});
 Product.belongsToMany(Cart, { through: CartItem});
 Order.belongsTo(User);
@@ -60,22 +81,30 @@ Order.belongsToMany(Product, { through: OrderItem});
 sequelize
 //.sync({ force: true})
 .sync()
-.then(result => {
-    const uid = 1;
-    return User.findAll({where: {id: uid}});
-})
-.then(([user]) => {
-    if (!user) {
-        return User.create({name: 'Max', email: 'max@gmail.com'});
-    }
-    return user;
-})
-.then(user => {
-    return user.createCart();
+// .then(result => {
+//     const uid = 1;
+//     return User.findAll({where: {id: uid}});
+// })
+// .then(([user]) => {
+//     const Sequelize = require('sequelize');
+//     if (!user) {
+//          const user = User.create({
+//             email: 'max@gmail.com', 
+//             password: 'Max',
+//             });
+//             //const login = sequelize.define('login', { email: Sequelize.STRING, hash: Sequelize.STRING })
+//             const hash = bcrypt.hashSync('Max', saltRounds);
+//             Login.create({ email: 'max@gmail.com', hash: hash })
+//             return user
+//     }
+//     return user;
+// })
+// .then(user => {
+//     return user.createCart();
     
-})
-.then(cart => {
-    app.listen(3000);
+// })
+.then(() => {
+    app.listen(3001);
 })
 .catch(err => {
     console.log(err);
